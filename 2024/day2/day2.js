52b94e61b3cbe96c193983f8e967f59d2069f6e8
Stephen Fiskell <stephen.fiskell@gmail.com>
2024-12-02 00:08:06 -0800

    WIP

--- 2024/day2/day2.js
+++ 2024/day2/day2.js
@@ -8,6 +8,7 @@ const MAX_DIFFER = 3;
 const INCREASING = "INCREASING";
 const DECREASING = "DECREASING";
 const safetyChecks = (cur, next, direction) => {
+    console.log('checking: ', cur, next, direction);
     if (next < cur && direction == INCREASING) {
         console.log('NOT SAFE: Should be increasing')
         return false;
@@ -28,60 +29,87 @@ const checkSafetyLevels = (input, dampen=false) => {
     let numSafe = 0;
     const reports = input.split('\n');
 
-    const isSafe = (report, withDampen=false) => {
+    const isSafe = (report, withDampen=false, hasDampened=false) => {
+        console.log('func IsSafe: ', report, withDampen, hasDampened);
         const parts = report.split(' ');
-        let direction = INCREASING;
-        let dampened = false;
+        let direction = null;
+        let dampened = hasDampened;
         for(let i = 0; i < parts.length-1; i++) {
             const cur = parseInt(parts[i]);
             const next = parseInt(parts[i+1]);
-
-            if (i == 0 && next < cur) {
-                direction = DECREASING;
+            
+            if(direction == null) {
+                direction = (next < cur) ? DECREASING : INCREASING;
             }
+            console.log(direction, cur, next);
 
             const safe = safetyChecks(cur, next, direction);
 
-            if(!safe && !withDampen) {
-                return false;
-            }
-
+            // We're good
             if(safe) {
                 continue;
             }
 
-            console.log('--Attempting to dampen: ', withDampen, dampened, i, parts.length-2);
+            // Not safe and not dampening (part 1)
+            if(!withDampen) {
+                console.log('NOT SAFE: dampening is off');
+                return false;
+            }
 
+            // Already dampened
             if(dampened) {
+                console.log('NOT SAFE: Already dampened');
                 return false;
             }
 
-            if(!safe && withDampen && !dampened && i < parts.length-2) {
-                const nextNext = parseInt(parts[i+2]);
-                let nextNextDir = INCREASING;
-                if (i == 0 && nextNext < cur) {
-                    nextNextDir = DECREASING;
-                }
+            console.log('--Attempting to dampen: ', withDampen, dampened, i, parts.length-2, direction);
 
-                let skipCurDir = INCREASING;
-                if (i == 0 && nextNext < next) {
-                    skipCurDir = DECREASING;
-                }
-
-                dampened = true;
-                console.log('--Dampening: ', cur, next, nextNext, nextNextDir, skipCurDir);
-                if (safetyChecks(cur, nextNext, nextNextDir)) {
-                    direction = nextNextDir;
-                    i += 1;
-                    continue;
-                } else if(safetyChecks(next, nextNext, skipCurDir)) {
-                    direction = skipCurDir;
-                    i += 1;
-                    continue;
-                }
-                console.log('--Dampening failed\n');
-                return false;
+            // Made it to the end, just remove the last element to "dampen"
+            if(i == parts.length-2) {
+                console.log('SAFE: Dampening last element');
+                return true;
             }
+
+            dampened = true;
+            const nextNext = parseInt(parts[i+2]); 
+
+            // // Set the dampened direction against the next next element
+            // let nextNextDir = direction;
+            // // Set the direction when skipping the current element
+            // let skipCurDir = direction;
+            // if(i == 0) {
+            //     nextNextDir = (nextNext < cur) ? DECREASING : INCREASING;
+            //     skipCurDir  = (nextNext < next) ? DECREASING : INCREASING;
+            // }
+
+            console.log('--Dampening: ', cur, next, nextNext);
+            // Call isSafe again with the 'next' element removed
+            const newReport = parts.slice(0, i + 1).concat(parts.slice(i + 2)).join(' ');
+            if (isSafe(newReport, withDampen, true)) {
+                console.log('SAFE: Dampening by removing element');
+                return true;
+            } 
+            // Call isSafe again with the 'cur' element removed
+            const newReport2 = parts.slice(0, i).concat(parts.slice(i + 1)).join(' ');
+            if (isSafe(newReport2, withDampen, true)) {
+                console.log('SAFE: Dampening by removing element');
+                return true;
+            }
+
+            // // Try to dampen to next next element
+            // if (safetyChecks(cur, nextNext, nextNextDir)) {
+            //     direction = nextNextDir;
+            //     i += 1;
+            //     continue;
+
+            // // Try to dampen next and next next (remove current)
+            // } else if(safetyChecks(next, nextNext, skipCurDir)) {
+            //     direction = skipCurDir;
+            //     i += 1;
+            //     continue;
+            // }
+            console.log('NOT SAFE: Dampening failed');
+            return false;
         }
         if(dampened) {
             console.log('Dampening successful\n');
@@ -90,10 +118,10 @@ const checkSafetyLevels = (input, dampen=false) => {
     }
 
     reports.map((report) => {
-        // console.log(`\n${report}`);
+        console.log(`\n${report}`);
         const safe = isSafe(report, dampen)
         if (safe) {
-            // console.log('SAFE: ', report);
+            console.log('SAFE: ', report);
             numSafe += 1;
         } else {
             console.log('NOT_SAFE: ', report, '\n');

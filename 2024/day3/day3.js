a489fce3b7a804b24a2bdd620c39f14bd95d9360
Stephen Fiskell <stephen.fiskell@gmail.com>
2024-12-02 22:19:11 -0800

    Clean up day 3

--- 2024/day3/day3.js
+++ 2024/day3/day3.js
@@ -5,7 +5,7 @@ const path = require('path');
 //xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 //Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
 
-const parse = (input) => {
+const solution_part1 = (input) => {
   const regex = /mul\(\d*[0-9],\d*[0-9]\)/g;
   const matches = input.matchAll(regex);
   let total = 0;
@@ -28,7 +28,7 @@ const runMul = (cmd) => {
   return left * right;
 };
 
-const parse2 = (input) => {
+const solution_part2 = (input) => {
   const regex = /mul\(\d*[0-9],\d*[0-9]\)/g;
   const do_regex = /do\(\)/g;
   const dont_regex = /don\'t\(\)/g;
@@ -37,21 +37,12 @@ const parse2 = (input) => {
   const do_matches = input.matchAll(do_regex);
   const dont_matches = input.matchAll(dont_regex);
 
-  // const d = [...dont_matches];
-  // console.log(d[0]['index']);
-
   const instructions = [...matches, ...do_matches, ...dont_matches];
-  // console.log(instructions);
-  // sort instructions
   instructions.sort((a, b) => {
     const aIndex = a['index'];
     const bIndex = b['index'];
     return aIndex - bIndex;
   });
-
-  console.log(instructions);
-
-  //Parse instructions
 
   let mul = true;
   let total = 0;
@@ -66,7 +57,6 @@ const parse2 = (input) => {
         break;
       default:
         if (mul) {
-          console.log('running: ', instruction[0]);
           total += runMul(instruction[0]);
         }
     }
@@ -76,7 +66,7 @@ const parse2 = (input) => {
 };
 
 // const input = fs.readFileSync(path.resolve(__dirname, 'part1-input.txt'), 'utf8')
-// console.log('Part 1 Answer: ', parse(input));
+// console.log('Part 1 Answer: ', solution_part1(input));
 
 const input2 = fs.readFileSync(path.resolve(__dirname, 'part1-input.txt'), 'utf8');
-console.log('Part 2 Answer: ', parse2(input2));
+console.log('Part 2 Answer: ', solution_part2(input2));

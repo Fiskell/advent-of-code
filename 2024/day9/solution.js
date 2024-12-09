3ad7ec19d45f926bed2635f750dbd32b007d3c8a
Stephen Fiskell <stephen.fiskell@gmail.com>
2024-12-08 23:23:54 -0800

    Day 9 Part 2

--- 2024/day9/solution.js
+++ 2024/day9/solution.js
@@ -66,22 +66,88 @@ const solution_part1 = (input) => {
   return sum(out);
 };
 
+const printMemory = (memory) => {
+  const out = memory.map((m) => m.val.join('')).join('');
+  console.log(out);
+};
+
+const rebalanceMemory = (files, memory, freeSpace) => {
+  for (let f = files.length - 1; f >= 0; f--) {
+    console.log('file: ', files[f]);
+    // console.log('\n Mem: ', memory, freeSpace);
+    const file = files[f];
+    if (file.moved) {
+      continue;
+    }
+    const size = parseInt(file.size);
+    //find index of first string of '.' that is the same size as the file
+    const index = freeSpace.indexOf(Array(size).fill('.').join(''));
+    // console.log('Index: ', index);
+    if (index === -1) {
+      continue;
+    }
+
+    if (index > file.index) {
+      continue;
+    }
+
+    // console.log(file.size, file);
+
+    // set new memory
+    memory.splice(index, size, ...Array(size).fill(file.id));
+
+    // add free space
+    freeSpace = freeSpace.slice(0, file.index) + Array(size).fill('.').join('') + freeSpace.slice(file.index + size);
+
+    // unset old memory
+    memory.splice(file.index, file.size, ...Array(parseInt(file.size)).fill('.'));
+
+    files[f].index = index;
+
+    // remove free space
+    freeSpace = freeSpace.slice(0, index) + Array(size).fill('#').join('') + freeSpace.slice(index + size);
+    files[f].moved = true;
+  }
+  return [files, memory];
+};
+
 const solution_part2 = (input) => {
-  const rows = input.split('\n');
-  let ret = 0;
-  rows.forEach((row) => {
-    const parts = row.split(' ');
-  });
-  return ret;
+  const parts = input.split('');
+  let id = 0;
+  let files = [];
+  let memory = [];
+  let freeSpace = [];
+  for (let i = 0; i < parts.length; i++) {
+    const isFile = i % 2 === 0;
+    const val = parts[i];
+
+    if (isFile) {
+      files.push({
+        id,
+        index: memory.length,
+        size: val,
+        moved: false,
+      });
+      memory = memory.concat(Array(parseInt(val)).fill(id));
+      freeSpace = freeSpace.concat(Array(parseInt(val)).fill('#'));
+      id++;
+    } else {
+      memory = memory.concat(Array(parseInt(val)).fill('.'));
+      freeSpace = freeSpace.concat(Array(parseInt(val)).fill('.'));
+    }
+  }
+  const [newFiles, newMemory] = rebalanceMemory(files, memory, freeSpace.join(''));
+  console.log(newFiles, newMemory);
+  return sum(newMemory);
 };
 
 // const input = fs.readFileSync(path.resolve(__dirname, 'sample.txt'), 'utf8');
 // const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
 // console.log('Part 1 Answer: ', solution_part1(input));
 
-const input2 = fs.readFileSync(path.resolve(__dirname, 'sample.txt'), 'utf8');
+// const input2 = fs.readFileSync(path.resolve(__dirname, 'sample.txt'), 'utf8');
 // const input2 = fs.readFileSync(path.resolve(__dirname, 'sample2.txt'), 'utf8');
-// const input2 = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
+const input2 = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
 console.log('Part 2 Answer: ', solution_part2(input2));
 
 module.exports = {
